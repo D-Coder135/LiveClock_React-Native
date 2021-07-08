25910e449d01625cdef9a303bb95d29c140e04d3
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-08 23:09:00 +0530

    update the text inside the Text tag

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.paragraph}>
-        Live Clock.
+        Live Clock
       </Text>
       <Card>
         <LiveClock />
