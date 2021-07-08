293e198de31339a2261f0f4309b1431c484d946c
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-08 22:03:56 +0530

    add the const styles field along with some properties inside it

--- components/LiveClock.js
+++ components/LiveClock.js
@@ -4,7 +4,26 @@ import {Text, View, StyleSheet} from 'react-native';
 export default class LiveClock extends React.Component {
     render() {
         return (
-            
+
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 24,
+    },
+    paragraph: {
+      margin: 24,
+      marginTop: 0,
+      fontSize: 14,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+    logo: {
+      height: 128,
+      width: 128,
+    }
+  });
