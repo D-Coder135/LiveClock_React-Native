c192b789a778828a5f5eab24c4ef93c0b8c00317
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-08 22:47:24 +0530

    add a View tag along with a Text tag inside it

--- components/LiveClock.js
+++ components/LiveClock.js
@@ -4,7 +4,11 @@ import {Text, View, StyleSheet} from 'react-native';
 export default class LiveClock extends React.Component {
     render() {
         return (
-
+             <View style={styles.container}>
+      <Text style={styles.paragraph}>
+        Local files and assets can be imported by dragging and dropping them into the editor
+      </Text>
+    </View>
         );
     }
 }
