1430e4336f2a2545ed3dd65bb74f3493f98647f5
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-08 21:59:56 +0530

    add render function inside the LiveClock class

--- components/LiveClock.js
+++ components/LiveClock.js
@@ -2,5 +2,7 @@ import * as React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 
 export default class LiveClock extends React.Component {
-    
+    render() {
+        
+    }
 }
