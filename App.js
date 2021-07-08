a009ca7503c90e1dc814d6350d1083c500640d22
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-08 23:09:32 +0530

    comment out some part of the code

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    // justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
     padding: 8,
